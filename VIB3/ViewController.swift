00e42e3fa4b1725001060538b936dc4ce6e565d8
natethancb <natethancb@gmail.com>
2020-09-22 09:38:14 -0400

    Initial Commit

---
+++ VIB3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VIB3
+//
+//  Created by Nathan Barta on 9/22/20.
+//  Copyright © 2020 Corectic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
